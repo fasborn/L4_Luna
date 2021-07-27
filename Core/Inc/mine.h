3eba334b2524901586af4d24b4ab93eb4ed2adef
fasborn <khetag.goiaev@gmail.com>
2021-07-28 01:22:53 +0300

    initial

---
+++ Core/Inc/mine.h
@@ -0,0 +1,9 @@
+#ifndef __MINE_H__
+#define __MINE_H__
+
+#include "main.h"
+
+ITStatus USART_GetITStatus(USART_TypeDef* USARTx, uint32_t USART_IT);
+void USART_ITConfig(USART_TypeDef* USARTx, uint32_t USART_IT, FunctionalState NewState);
+
+#endif
